d282e917c188d4147cbec3e67eebbee2b11c0c65
Hsieh-hulk <Hsieh-hulk@users.noreply.github.com>
2026-08-02 22:12:28 +0800

    Fix Tailwind CSS v4 build syntax error

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,7 @@ export default {
         sans: ['"Plus Jakarta Sans"', '"Noto Sans TC"', 'sans-serif'],
       },
       screens: {
-        'ipad-portrait': { 'raw': '(min-width: 768px) and (max-width: 1024px) and (orientation: portrait)' },
-        'ipad-landscape': { 'raw': '(min-width: 1024px) and (max-width: 1280px)' },
+
       }
     }
   },
